dee243e85f58ef4dbb133423be551a6c7903176c
Priyanka <priyankaag.0613@gmail.com>
2023-12-27 22:18:52 +0530

    float

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        //int 
-        findMax p1=new findMax();
-        p1.findMaxNumber(1,3,4);
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter float number 1: ");
+        float num1=s.nextInt();
+        System.out.println("Enter float number 2: ");
+        float num2=s.nextInt();
+        System.out.println("Enter float number 3: ");
+        float num3=s.nextInt();
+
+        //double
+        findMax p2=new findMax();
+        p2.findMaxNumber(num1,num2,num3);
+        s.close();
     }
 }
--- src/findMax.java
+++ src/findMax.java
@@ -9,4 +9,15 @@ public class findMax{
         }
         System.out.println(max);
     }
+
+    public void findMaxNumber (Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        System.out.println(max);
+    }
 }
